bed6e08e63a0ea00fa515caa591be054a0ba9d17
issamL <lhous.issam99@gmail.com>
2021-05-12 15:56:22 +0200

    adding code smells

--- sketch.js
+++ sketch.js
@@ -3,6 +3,11 @@ function sum(a, b) {
     }else{
         return a + b;
     }
-
+}
+function sum(a, b) {
+    if(0){console.log("test")
+    }else{
+        return a + b;
+    }
 }
 module.exports = sum;
